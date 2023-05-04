68531fb712f7514543f916b3bc25b44ab4b7ac88
bear-muna <bearmuna@gmail.com>
2023-05-04 16:42:19 -0700

    added repetitive code for create and update products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -39,19 +39,6 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const newProduct = await Product.create({
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      tagIds: req.body.tagIds,
-    });
-    res.json(newProduct);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Error occurred", error })
-  }
-  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -75,7 +62,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
